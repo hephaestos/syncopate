a99c99771f12a7176f40a474c1f8dac7fe4d4fab
Hephaestos <thedanielfloyd@protonmail.com>
2021-02-04 16:55:16 -0500

    initial babel, webpack, react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import {hot} from "react-hot-loader";
+import  "./App.css";
+
+class App extends Component{
+    render() {
+        return(
+            <div classname="App">
+                <h1> Syncopate </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
