25b5a6425b8a6d89ba945a6d32710a6ce9c2a933
Hephaestos <thedanielfloyd@protonmail.com>
2021-02-04 18:18:59 -0500

    add sass and restructure folders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,34 +1,74 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/env"] }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
+module.exports = (env,argv) => {
+    const isDevelopment = argv.mode !== 'production';
+    const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+    
+    return {
+        entry: "./src/index.js",
+        mode: "development",
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: "babel-loader",
+                    options: { presets: ["@babel/env"] }
+                },
+                {
+                    test: /\.module\.s(a|c)ss$/,
+                    use: [
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                sourceMap: isDevelopment
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.s(a|c)ss$/,
+                    exclude: /\.module.(s(a|c)ss)$/,
+                    use: [
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        resolve: { extensions: ["*", ".js", ".jsx", ".scss"] },
+        output: {
+            path: path.resolve(__dirname, "dist/"),
+            publicPath: "/dist/",
+            filename: "bundle.js"
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "public/"),
+            port: 3000,
+            publicPath: "http://localhost:3000/dist/",
+            hotOnly: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new MiniCssExtractPlugin({
+                filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+                chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+            })
         ]
-    },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        publicPath: "http://localhost:3000/dist/",
-        hotOnly: true
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
-};
+    }
+}
