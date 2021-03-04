bac9e82cb9cdaa100c577c0b2f9639256f6862db
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2021-03-03 22:02:06 -0500

    Just added some notes about possible PATHs and POST/GET requests we will need to implement

--- server/syncserver.js
+++ server/syncserver.js
@@ -11,3 +11,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+//Maybe put PATH constants up here so requests can be made for specific pages / information
+//Example: Session path -->  /:uid/:sessionid
+
+
+//POST request here for creating a new session
+
+//GET(?) request for joining an existing session
