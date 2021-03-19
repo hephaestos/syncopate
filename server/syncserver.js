fc8d0ee83fce087692253eaa603c4c5b215499cc
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2021-03-18 20:04:11 -0400

    Added two example get HTTP methods which create/join pseudo sessions. Still need to implement sessions middleware

--- server/syncserver.js
+++ server/syncserver.js
@@ -1,21 +1,39 @@
 // Initial backend with example 'Hello World' function from Postman
 
 const express = require('express')
+var parseurl = require('parseurl')
+var session = require('express-session')
 const app = express()
 const port = 4000
+var identifier = 0
+let currSessions = {}
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Syncopate!!')
-})
+
+//Maybe put PATH constants up here so requests can be made for specific pages / information
+//Example: Session path -->  /:uid/:sessionid
+const createPath = '/create-session/:uid'
+const joinPath = '/join-session/:sid'
+
+//POST request here for creating a new session.
+//@params -> POST request should include userID of some kind (maybe Spotify login name/email?)
+app.get(createPath, (req, res) => {
+  if (currSessions[`${req.params.uid}`] == null) {
+    currSessions[`${req.params.uid}`] = `${req.params.uid + identifier++}`
+  }
+  res.send(`You just created a session with unique ID: ${currSessions[req.params.uid]}`)
+});
+
+//GET(?) request for joining an existing session
+app.get(joinPath, (req, res) => {
+  Object.keys(currSessions).forEach(function(key) {
+  if(currSessions[key] == req.params.sid) {
+    res.send('This session exists!')
+  }
+});
+  //res.send('This session does not exist!')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-//Maybe put PATH constants up here so requests can be made for specific pages / information
-//Example: Session path -->  /:uid/:sessionid
-
-
-//POST request here for creating a new session
-
-//GET(?) request for joining an existing session
