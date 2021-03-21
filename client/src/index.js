3d78534f9e4704164851b376c987b1eb3b8a842b
Hephaestos <thedanielfloyd@protonmail.com>
2021-03-20 20:10:05 -0400

    add bootstrap correctly

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
-//React App
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
-
-//Styles
-import 'bootstrap/dist/css/bootstrap.css';
 import './scss/global'
-
 require('file-loader?name=[name].[ext]!./index.html');
 ReactDOM.render(<App />, document.getElementById("root"));
