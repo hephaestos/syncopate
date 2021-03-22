2059993ab739c9942830e878f8add6c1b1122d8c
jacobwrestlesangels <jacobmjohnson99@gmail.com>
2021-03-22 14:56:17 -0400

    Added session scss file and worked on some responsiveness

--- client/src/components/Userbar.js
+++ client/src/components/Userbar.js
@@ -4,7 +4,7 @@ class UserBar extends Component {
     render() {
         return (
             <div className="UserBar">
-                <h3>Users</h3>
+                <h3 className="UserBar__title">Users</h3>
                 <hr></hr>
                 <User profPic={"./images/logo.svg"} username={"Jacob Johnson"}/>
                 <User profPic={"./images/mips.png"} username={"Jacob Johnson"}/>
