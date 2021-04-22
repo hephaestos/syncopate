1c056013f7c1705f4fb21c5abb14ebceef22bef5
thombran <thombran@mail.gvsu.edu>
2021-04-22 16:18:51 -0400

    Small changes I think

--- server/syncserver.js
+++ server/syncserver.js
@@ -209,6 +209,7 @@ io.on('connection', async (socket) => {
         // Make sure user has not already started hostng a session. If so, send an error message
         if (userSessionExists != null) {
             console.log('User cannot posses more than one session');
+            // Error message sent to user if they already have session
             io.to(sessionID).emit('create session', 'Error: User already created session');
         // Otherwise, create a session on the backend
         } else {
@@ -227,7 +228,7 @@ io.on('connection', async (socket) => {
                 .updateOne({ _id: socket.id },
                     { $set: { currSession: sessionID } });
             console.log(`Session created with sessionID: ${sessionID}`);
-            io.to(sessionID).emit('create session', sessionID);
+            io.to(sessionID).emit('create session', sessionID); // Send back session name to user
         }
         socket.join(sessionID); // Add this user/socket to a room with their session ID
     });
