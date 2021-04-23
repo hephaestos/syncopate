7cd5e6cf7fc1637391944cdcfd557ef9defd4811
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2021-04-23 12:39:43 -0400

    Added comments/documentation for most of backend

--- server/syncserver.js
+++ server/syncserver.js
@@ -6,27 +6,30 @@
  * @version 0.0.1
  * @description This is the API for the Syncopate client which handles user requests to either
  * create, join, or destroy music listening sessions. The API stores user session data within a
- * MongoDB cluster. The POST method ensures user is creating unique session name, and GET method
- * to join a session ensures session already exists and user has provided valid password.
+ * MongoDB cluster. The IO 'connection' ensures user is creating unique session name, and the IO
+ * 'Join Session' to join a session ensures session already exists and user has provided
+ * valid password.
  */
 import express from 'express'; // Express API library
 import session from 'express-session'; // Used to store and manage user sessions
 import MongoStore from 'connect-mongo'; // Database for backend storage of user data
 import mongoose from 'mongoose'; // Allows for use of NoSQL commands to pull from Mongo DB
-import querystring from 'querystring';
-import request from 'request';
+import querystring from 'querystring'; // Used to make queries to Spotify
+import request from 'request'; // Make requests to Spotify API
 
 import * as http from 'http';
 import * as socketio from 'socket.io'; // Module to handle Syncopate music sessions
 import {
     sessionSecret, URL, client_id, client_secret,
-} from './secrets.js'; // Server/backend secrets for server consturctor
+} from './secrets.js'; // Server/backend secrets for server consturctor and client secrets
 
 import { SyncSessionModel, uniqueID } from './syncSessionModel.js'; // Model for Syncopate sessions and generating IDs
 
 // Constructing express server using sockets.io
 const app = express();
 const server = http.createServer(app);
+
+// Client URIs
 const redirect_uri = 'http://localhost:4000/callback';
 const frontend_uri = 'http://localhost:3000';
 const stateKey = 'spotify_auth_state';
@@ -36,7 +39,7 @@ const io = new socketio.Server(server, {
         origin: frontend_uri,
         methods: ['GET', 'POST'],
     },
-    pingTimeout: 60000,
+    pingTimeout: 60000, // Ping timeout and interval allow for longer socket requests
     pingInterval: 25000,
 });
 
@@ -61,20 +64,18 @@ app.use(session({
     store: new MongoStore({
         mongoUrl: URL,
         dbName: 'Syncopate',
-        collectionName: 'express', // Where user IDs will be stored
+        collectionName: 'express', // Where express user IDs will be stored
         autoRemove: 'native', // Default value for auto remove
         mongooseConnection: db,
     }),
 })).use(express.static('public'));
 
 /**
- * Test route for site landing page in order to test various API functionality. Can be removed
- * for release
+ * @summary Route to redirect user to Spotify site to login and authorize (User needs premium)
+ * @param req The request from the client side containing headers, etc.
+ * @param res The response to send back to the client
+ * @returns Sends user to Spotify domain
  */
-app.get('/', (req, res) => {
-    res.sendFile('index.html'); // Load HTML page
-});
-
 app.get('/login', (req, res) => {
     // your application requests authorization
     const scope = 'user-read-private user-read-email streaming user-read-playback-state user-modify-playback-state user-library-read user-library-modify';
@@ -87,10 +88,10 @@ app.get('/login', (req, res) => {
         })}`);
 });
 
+/**
+ *
+ */
 app.get('/callback', (req, res) => {
-    // your application requests refresh and access tokens
-    // after checking the state parameter
-
     const code = req.query.code || null;
 
     res.clearCookie(stateKey);
@@ -138,6 +139,9 @@ app.get('/callback', (req, res) => {
     });
 });
 
+/**
+ *
+ */
 app.get('/refresh_token', (req, res) => {
     // requesting access token from refresh token
     const { refreshToken } = req.query;
@@ -167,19 +171,19 @@ app.get('/refresh_token', (req, res) => {
  * database. The user has the ability to create, join, or leave rooms using their socket
  * connection. There is also a test socket listener to test sending "messages" between a
  * group of users in a room.
- * @emits chat_message Sends message back to users when message is received. For testing
+ * @param socket Socket connection from client side
  */
 io.on('connection', async (socket) => {
     try {
-        // Grab the unique user ID from the socket header being sent to the server
-        const newUserID = socket.id;
+        const newUserID = socket.id; // User ID is set as the unique socket ID
+
         // Find user in database and make sure current session is set to null until they join a room
         await db
             .collection('UIDs')
             .insertOne({
                 _id: newUserID,
-                spotifyID: null,
-                created: new Date(),
+                spotifyID: null, // User has not logged in to Spotify yet
+                created: new Date(), // Set date as now
                 currSession: null,
             });
         console.log(`Connected user: ${newUserID}`);
@@ -187,31 +191,29 @@ io.on('connection', async (socket) => {
         console.log(`Caught an error: ${e}`);
     }
 
-    // Test method to send messages to all users connected to the server
-    socket.on('chat message', (msg) => {
-        console.log(msg);
-        io.emit('chat message', msg); // Send msg back to all users
-    });
-
     /**
-     * Method to create a unique session for the user and add themselves to the room.
-     * For production/implementation of Spotify need to send session name back to socket
+     * @description Method to create a unique session for the user and add themselves to the room.
+     * Also sends the session name/code back to the user via socket.emit to session name
+     * can be given to those who want to join the room
+     * @emit Sends unique session ID back to user who created session
+     * @emit Sends error back to client if user already has a session under their ID
      */
     socket.on('create session', async () => {
         let sessionID = uniqueID(); // Uniquely generated session ID
         const query = await db.collection('sessions').findOne({ _id: sessionID }); // See if session ID exists in DB
 
         // If we did not generate a unique ID, generate another. TODO: **Replace this method**
-        if (query != null) sessionID = uniqueID();
+        if (!query) sessionID = uniqueID();
 
-        const userID = socket.id; // Grab userID from global map
-        const spotUsername = await db.collection('UIDs').findOne({ id: socket.id }).spotifyID;
-        // Create new session model for this user
+        const userID = socket.id; // User ID is socket ID
+        const spotUsername = await db.collection('UIDs').findOne({ id: socket.id }).spotifyID; // Grab Spotify ID
+
+        // Create new session model for this user, initialize session owner, and add them to users
         const userSession = new SyncSessionModel(userID, spotUsername);
         const userSessionExists = await db.collection('sessions').findOne({ 'userSession.uid': userID });
 
         // Make sure user has not already started hostng a session. If so, send an error message
-        if (userSessionExists != null) {
+        if (userSessionExists) {
             console.log('User cannot posses more than one session');
             // Error message sent to user if they already have session
             io.to(sessionID).emit('create session', 'Error: User already created session');
@@ -226,27 +228,27 @@ io.on('connection', async (socket) => {
             (error) => {
                 if (error) console.log(error);
             });
+
             // Change user's current session to newly created session ID
-            await db
-                .collection('UIDs')
-                .updateOne({ _id: socket.id },
-                    { $set: { currSession: sessionID } });
+            await db.collection('UIDs').updateOne({ _id: socket.id }, { $set: { currSession: sessionID } });
             console.log(`Session created with sessionID: ${sessionID}`);
+
             io.to(sessionID).emit('create session', sessionID); // Send back session name to user
         }
         socket.join(sessionID); // Add this user/socket to a room with their session ID
     });
 
     /**
-     * Method which is called automatically when the user disconnects from a session
+     * @description Method which is called automatically when the user disconnects from a session
      * (i.e. they close out of their browser or by other means). Removes socket/user from their
      * current session automatically. If the session is empty, deletes the session from the DB.
      */
     socket.on('disconnect', async (reason) => {
-        const disID = socket.id; // The actual randomly generated userID
+        const disID = socket.id;
         try {
             const currUser = await db.collection('UIDs').findOne({ _id: disID }); // Grab Promise of user from DB, if they exist
             const currSess = currUser.currSession; // Current session user is in
+
             if (currSess) {
                 let userInSess = await db.collection('sessions').findOne({ _id: currSess }); // See if session user is in exists
                 if (userInSess) { // If user is in actual session
@@ -254,6 +256,7 @@ io.on('connection', async (socket) => {
                     await db.collection('sessions').updateOne({ _id: currSess }, { $pull: { 'userSession.users': { userID: disID } } });
                     userInSess = await db.collection('sessions').findOne({ _id: currSess }); // Refresh user list
                     socket.leave(currSess); // Remove user from socket room
+
                     // If session is empty when user leaves, delete it
                     if (userInSess.userSession.users.length === 0) {
                         await db.collection('sessions').deleteOne({ _id: currSess });
@@ -264,19 +267,25 @@ io.on('connection', async (socket) => {
         } catch (e) {
             console.log('Failed to find user with this ID');
         }
-        console.log(`User ${disID} disconnected with reason: ${reason}`);
+        console.log(`User ${disID} disconnected with reason: ${reason}`); // Disconnect reasoning logged
     });
 
     /**
-     * Method for users to be able to join other, existing sessions. Checks to make sure
-     * the session exists, and if it does, add user's socket to room and adds them to room's DB
+     * @description Method for users to be able to join other, existing sessions.
+     * Checks to make sure the session exists, and if it does, add user's socket
+     * to room and adds them to room's DB.
+     * @emit Sends list of all users' Spotify usernames who are currently connected to room, back
+     * to everyone in that room
+     * @param sessionName The session ID/name which is being joined by the client
      */
     socket.on('join session', async (sessionName) => {
         const currID = socket.id;
         try {
             const currUser = await db.collection('UIDs').findOne({ _id: currID }); // Make sure user exists
+
             if (currUser) {
                 const reqSession = await db.collection('sessions').findOne({ _id: sessionName });
+
                 // If session user is looking for exists, add them to the session's list of users
                 if (reqSession) {
                     // If user exists, change their current session to the one they are joining
@@ -291,10 +300,12 @@ io.on('connection', async (socket) => {
                     let usersInSession = await db.collection('sessions').findOne({ _id: sessionName });
                     usersInSession = usersInSession.userSession.users; // Grab user objects in array
                     const users = [];
+
                     // For each user, append their username to array
                     usersInSession.array.forEach((element) => {
                         users.push(element.spotName);
                     });
+
                     // Send usernames back to clients in room user joined
                     io.to(sessionName).emit('join session', users);
                 }
@@ -304,6 +315,11 @@ io.on('connection', async (socket) => {
         }
     });
 
+    /**
+     * @description Sends request to Spotify API to authenticate client. Then, sends
+     * the client's Spotify username back to the client, which is later used in the DB
+     * @emit Sends client's Spotify username back to client upon success
+     */
     socket.on('get spotify id', async (access_token) => {
         const options = {
             url: 'https://api.spotify.com/v1/me',
@@ -320,7 +336,9 @@ io.on('connection', async (socket) => {
             console.log('body');
             console.log(body);
 
-            io.to(socket.id).emit('get spotify id', body);
+            io.to(socket.id).emit('get spotify id', body); // Send Spotify username to client
+
+            // Update client's spotifyID field in databse with their username
             db.collection('UIDs').updateOne({ _id: socket.id }, { $set: { spotifyID: body.id } });
         });
     });
